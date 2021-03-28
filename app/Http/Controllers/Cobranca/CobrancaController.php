23b5ad5f3cb4412a0ec73ab52ddcd003f400b535
Juan Calavera <juanconecti@gmail.com>
2021-03-28 13:04:12 -0300

    Create Payment Controller

--- app/Http/Controllers/Cobranca/CobrancaController.php
+++ app/Http/Controllers/Cobranca/CobrancaController.php
@@ -25,7 +25,132 @@ class CobrancaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, config('app.asaas_url') . "payments");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+
+        curl_setopt($ch, CURLOPT_POSTFIELDS, '{
+  "customer": "' . $request->customer . '",
+  "billingType": "BOLETO",
+  "dueDate": "' . $request->dueDate . '",
+  "value": ' . $request->value . ',
+  "description": "' . $request->description . '",
+  "externalReference": "' . $request->externalReference . '",
+  "discount": {
+    "value": ' . $request->value1 . ',
+    "dueDateLimitDays": ' . $request->dueDateLimitDays . '
+  },
+  "fine": {
+    "value": ' . $request->value2 . '
+  },
+  "interest": {
+    "value": ' . $request->value3 . '
+  },
+  "postalService": ' . $request->postalService . '
+}');
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            "Content-Type: application/json",
+            "access_token: " . config("app.asaas_key")
+        ));
+
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        return $response;
+    }
+
+    public function storeCartao(Request $request)
+    {
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, config('app.asaas_url') . "payments/cartao");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+
+        curl_setopt($ch, CURLOPT_POSTFIELDS, '{
+        "customer": "' . $request->customer . '",
+        "billingType": "CREDIT_CARD",
+        "dueDate": "' . $request->dueDate . '",
+        "value": ' . $request->value . ',
+        "description": "' . $request->description . '",
+        "externalReference": "' . $request->externalReference . '",
+        "creditCard": {
+            "holderName": "' . $request->holderName . '",
+            "number": "' . $request->number . '",
+            "expiryMonth": "' . $request->expiryMonth . '",
+            "expiryYear": "' . $request->expiryYear . '",
+            "ccv": "' . $request->ccv . '"
+        },
+        "creditCardHolderInfo": {
+            "name": "' . $request->name . '",
+            "email": "' . $request->email . '",
+            "cpfCnpj": "' . $request->cpfCnpj . '",
+            "postalCode": "' . $request->postalCode . '",
+            "addressNumber": "' . $request->addressNumber . '",
+            "addressComplement": ' . $request->addressComplement . ',
+            "phone": "' . $request->phone . '",
+            "mobilePhone": "' . $request->mobilePhone . '"
+        },
+        "creditCardToken": "' . $request->creditCardToken . '"
+        }');
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            "Content-Type: application/json",
+            "access_token: " . config('app.asaas_key')
+        ));
+
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        return response()->json($response);
+    }
+
+    public function storeParcelada(Request $request)
+    {
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, config('app.asaas_url') . "payments/parcelado");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+
+        curl_setopt($ch, CURLOPT_POSTFIELDS, '{
+  "customer": "' . $request->customer . '",
+  "billingType": "' . $request->billingType . '",
+  "installmentCount": ' . $request->installmentCount . ',
+  "installmentValue": ' . $request->installmentValue . ',
+  "dueDate": "' . $request->dueDate . '",
+  "description": "' . $request->description . '",
+  "externalReference": "' . $request->externalReference . '",
+  "discount": {
+    "value": ' . $request->value . ',
+    "dueDateLimitDays": ' . $request->dueDateLimitDays . '
+  },
+  "fine": {
+    "value": ' . $request->valueMulta . '
+  },
+  "interest": {
+    "value": ' . $request->valueJuros . '
+  }
+}');
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            "Content-Type: application/json",
+            "access_token: " . config('app.asaas_key')
+        ));
+
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        return $response;
     }
 
     /**
@@ -36,7 +161,21 @@ class CobrancaController extends Controller
      */
     public function show($id)
     {
-        //
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, config('app.asaas_url') . "payments/" . $id);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            "Content-Type: application/json",
+            "access_token: " . config('app.asaas_key')
+        ));
+
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        return $response;
     }
 
     /**
@@ -48,7 +187,42 @@ class CobrancaController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, config('app.asaas_url') . "payments/" . $id);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+
+        curl_setopt($ch, CURLOPT_POSTFIELDS, '{
+  "billingType": "BOLETO",
+  "dueDate": "2017-06-10",
+  "value": 100,
+  "description": "Pedido 056984",
+  "externalReference": "056984",
+  "discount": {
+    "value": 10,
+    "dueDateLimitDays": 0
+  },
+  "fine": {
+    "value": 1
+  },
+  "interest": {
+    "value": 2
+  },
+  "postalService": false
+}');
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            "Content-Type: application/json",
+            "access_token: " . config('app.asaas_key')
+        ));
+
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        var_dump($response);
     }
 
     /**
@@ -58,6 +232,59 @@ class CobrancaController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
+    {
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, config('app.asaas_url') . "payments/" . $id);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            "Content-Type: application/json",
+            "access_token: " . config('app.asaas_key')
+        ));
+
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        var_dump($response);
+    }
+
+    public function restore($id)
+    {
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, config('app.asaas_url') . "payments/{id}/restore
+");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            "Content-Type: application/json",
+            "access_token: sua_api_key"
+        ));
+
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        var_dump($response);
+    }
+
+    public function estornar($id)
+    {
+        //
+    }
+
+    public function receiveInCash($id)
+    {
+        //
+    }
+
+    public function dereceiveCash($id)
     {
         //
     }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Cliente\ClienteController;
+use App\Http\Controllers\Cobranca\CobrancaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use PhpParser\Node\Expr\postDec;
@@ -15,92 +16,47 @@ use PhpParser\Node\Expr\postDec;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::prefix('customers')->group(function () {
+    // Criar Cliente
+    Route::post('/', [ClienteController::class, 'store']);    
+    // Recuperar um único cliente
+    Route::get('/{id}', [ClienteController::class, 'show']);
+    // Listar clientes
+    Route::get('/', [ClienteController::class, 'index']);
+    // Atualizar cliente existente
+    Route::patch('/{id}', [ClienteController::class, 'update']);
+    // Remover cliente
+    Route::delete('/{id}', [ClienteController::class, 'destroy']);
+    // Restaurar cliente removido
+    Route::post('/{id}/restore', [ClienteController::class, 'restore']);
+});
 
-// Criar Cliente
-// customers - post
-Route::post('/customers', [ClienteController::class, 'store']);
-
-// Recuperar um único cliente
-// get
-// customers/{id}
-Route::get('/customers/{id}', [ClienteController::class, 'show']);
-
-// Listar clientes
-// get
-// customers?name=&email=&cpfCnpj=&groupName=&externalReference=&offset=&limit=
-Route::get('/customers', [ClienteController::class, 'index']);
-
-// Atualizar cliente existente
-// post
-// customers/{id}
-Route::patch('/customers/{id}', [ClienteController::class, 'update']);
-
-// Remover cliente
-// delete
-// customers/{id}
-Route::delete('/customers/{id}', [ClienteController::class, 'destroy']);
-
-// Restaurar cliente removido
-// post
-// customers/{id}/restore
-Route::post('/customers/{id}/restore', [ClienteController::class, 'restore']);
-
-// Criar nova cobrança
-// post
-// payments
-Route::post('/payments-boleto', function () {return 'Criar nova cobrança';});
-
-// Criar cobrança com cartão de crédito
-// post
-// payments
-Route::post('/payments-cartao', function () {return 'Criar cobrança com cartão de crédito';});
-
-// Criar uma cobrança parcelada
-// post
-// payments
-Route::post('/payments-parcelado', function () {return 'Criar uma cobrança parcelada';});
+Route::prefix('payments')->group(function () {
+    // Criar nova cobrança    
+    Route::post('/boleto', [CobrancaController::class, 'store']);
+    // Criar cobrança com cartão de crédito
+    Route::post('cartao', [CobrancaController::class, 'storeCartao']);
+    // Criar uma cobrança parcelada
+    Route::post('parcelado', [CobrancaController::class, 'storeParcelada']);
+    // Recuperar uma única cobrança
+    Route::get('/{id}', [CobrancaController::class, 'show']);
+    // Listar cobranças
+    Route::get('?customer=&billingType=&status=&subscription=&installment=&externalReference=&paymentDate=&anticipated=&paymentDate%5Bge%5D=&paymentDate%5Ble%5D=&dueDate%5Bge%5D=&dueDate%5Ble%5D=&offset=&limit=', function () {return 'Listar cobranças';});
+    // Atualizar cobrança existente
+    Route::patch('/{id}', [CobrancaController::class, 'update']);
+    // Remover cobrança
+    Route::delete('/{id}', [CobrancaController::class, 'destroy']);
+    // Restaurar cobrança removida
+    Route::post('/{id}/restore', [CobrancaController::class, 'restore']);
+    // Estornar cobrança
+    Route::post('/{id}/refund', [CobrancaController::class, 'estornar']);
+    // Confirmar recebimento em dinheiro
+    Route::post('/{id}/receiveInCash', [CobrancaController::class, 'receiveInCash']);    
+    // Desfazer confirmação de recebimento em dinheiro
+    Route::post('/{id}/receiveInCash', [CobrancaController::class, 'dereceiveCash']);
+});
 
 // Criar cobrança com Split
 // post
 // payments
 // Route::post('/payments', function () {return 'Criar cobrança com Split';});
-
-// Recuperar uma única cobrança
-// get
-// payments/{id}
-Route::get('/payments/{id}', function () {return 'Recuperar uma única cobrança';});
-
-// Listar cobranças
-// get
-// payments?customer=&billingType=&status=&subscription=&installment=&externalReference=&paymentDate=&anticipated=&paymentDate%5Bge%5D=&paymentDate%5Ble%5D=&dueDate%5Bge%5D=&dueDate%5Ble%5D=&offset=&limit=
-Route::get('/payments?customer=&billingType=&status=&subscription=&installment=&externalReference=&paymentDate=&anticipated=&paymentDate%5Bge%5D=&paymentDate%5Ble%5D=&dueDate%5Bge%5D=&dueDate%5Ble%5D=&offset=&limit=', function () {return 'Listar cobranças';});
-
-// Atualizar cobrança existente
-// post
-// payments/{id}
-Route::patch('/payments/{id}', function () {return 'Atualizar cobrança existente';});
-
-// Remover cobrança
-// delete
-// payments/{id}
-Route::delete('/payments/{id}', function () {return 'Remover cobrança';});
-
-// Restaurar cobrança removida
-// post
-// payments/{id}/restore
-Route::post('/payments/{id}/restore', function () {return 'Restaurar cobrança removida';});
-
-// Estornar cobrança
-// post
-// payments/{id}/refund
-Route::post('/payments/{id}/refund', function () {return 'Estornar cobrança';});
-
-// Confirmar recebimento em dinheiro
-// post
-// payments/{id}/receiveInCash
-Route::post('/payments/{id}/receiveInCash', function () {return 'Confirmar recebimento em dinheiro';});
-
-// Desfazer confirmação de recebimento em dinheiro
-// post
-// payments/{id}/receiveInCash
-Route::post('/payments/{id}/receiveInCash', function () {return 'Desfazer confirmação de recebimento em dinheiro';});
